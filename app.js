64e80bf6781c1541a213ff6e18d034b4f63212b4
tomhon <tomhon@microsoft.com>
2016-05-20 17:11:42 -0700

    added array search

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 var restify = require('restify');
 
-// var PartnerISV = [
-//     ['Snapchat Inc'],['James Cadd <jacadd@microsoft.com>'],
-//     ['Twitter Inc'],['Chris Barker <cbarker@microsoft.com>'],
-//     ['Yahoo!'],['Maarten van de Bospoort <maartenb@microsoft.com>'],
-//     ['baidu'],['Yansong Li <yansongl@microsoft.com>'],    
-// ];
+var partnerISV = new Array(5);
+
+// for ( var i = 1; i < 5; i++) 
+//     {
+//         partnerISV[i] = new Array(2);
+//         console.log(i);
+//     };
+
+ 
+
+partnerISV.forEach(function(item) {
+    partnerISV[item] = new Array(2);
+});
+partnerISV[0]= new Array(2);
+partnerISV[0][0] = 'Snapchat Inc';
+partnerISV[0][1] = 'James Cadd <jacadd@microsoft.com>';
+partnerISV[1]= new Array(2);
+partnerISV[1][0] = 'Twitter Inc';
+partnerISV[1][1] = 'Chris Barker <cbarker@microsoft.com>';
+partnerISV[2]= new Array(2);
+partnerISV[2][0] = 'Yahoo!';
+partnerISV[2][1] = 'Maarten van de Bospoort <maartenb@microsoft.com>';
+partnerISV[3]= new Array(2);
+partnerISV[3][0] = 'baidu';
+partnerISV[3][1] = 'Yansong Li <yansongl@microsoft.com>';
+// partnerISV[1].account(['Twitter Inc'],
+// partnerISV[1].TE['Chris Barker <cbarker@microsoft.com>']);
+// partnerISV[2].account(['Yahoo!'],
+// partnerISV[2].TE['Maarten van de Bospoort <maartenb@microsoft.com>']);
+// partnerISV[3].account(['baidu'],
+// partnerISV[3].TE['Yansong Li <yansongl@microsoft.com>']);
 
 var arrayIsvTE = [];
 var cacheDataAvailable = false;
@@ -23,8 +48,7 @@ var Connection = require('tedious').Connection;
         // If no error, then good to proceed.
         console.log("Connected");
         arrayIsvTE.push("First Item on the mapping array");
-        executeStatement();
-                console.log("Cache data available " + cacheDataAvailable);
+        // executeStatement();
         console.log("Mapping Array length = " + arrayIsvTE.length);
         // console.log("Output from function " + PartnerISV);
 
@@ -69,11 +93,42 @@ var Connection = require('tedious').Connection;
 
 // Setup Restify Server
 var server = restify.createServer();
+var pos = 0;
+
+partnerISV.forEach(function (item) {
+    arrayData = item;
+    arrayData.forEach(function(item) {
+        nestedArrayData = item;
+        console.log(nestedArrayData);
+        if (nestedArrayData == 'Yahoo!') {
+            console.log("Hit");
+        };
+    });
+    });
+ var x = 0;
+ var account = 'Yahoo!'
+console.log("Looking for account");
+while ( x < partnerISV.length) {
+    console.log(x);
+    if (partnerISV[x][0] == account) {
+        console.log(partnerISV[x][0] +" " + partnerISV[x][1]);
+        x = partnerISV.length;
+    };
+    x++;
+    };
+
+ 
+    // pos = item.search('Yahoo!');
+    // console.log(pos);
+
+
 
 
 server.listen(process.env.port || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
     console.log(arrayIsvTE.length); 
-    console.log("Cache data available " + cacheDataAvailable);
+
+    
+
 
 });
